65f4fdadf8eb83c25859d6698cb1e3aa8df4fdf9
sega16322 <aventador_849@docomo.ne.jp>
2020-05-31 20:53:07 +0900

    一覧機能に関する記述をNotificationコントローラに記述

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,17 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = [
-    @user = User.find(params[:id])
-    @item = @user.find(id: @item.saler_id)
-    ]
+    #current_userの投稿に紐づいた通知一覧
+      @notifications = current_user.passive_notifications
+    #@notificationの中でまだ確認していない(indexに一度も遷移していない)通知のみ
+      @notifications.where(checked: false).each do |notification|
+          notification.update_attributes(checked: true)
+      end
   end
+
+  def destroy_all
+    #通知を全削除
+      @notifications = current_user.passive_notifications.destroy_all
+      redirect_to users_notifications_path
+  end
+
 end
