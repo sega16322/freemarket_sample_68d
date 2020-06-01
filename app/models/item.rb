7004e5818b5d2b5bcae8c8f3001dac4c746581aa
sega16322 <aventador_849@docomo.ne.jp>
2020-06-01 21:39:35 +0900

    変数名、引数をわかりやすく修正
    commentの名詞はcommentatorだろって思う人もいるかも知れませんが、辞典のOxfordによると、commentatorは公の場でその場にふさわしい地位でコメントをする人のことを言うらしい。commenterはネット上などでコメントする人を指す。

--- app/models/item.rb
+++ app/models/item.rb
@@ -35,6 +35,7 @@ class Item < ApplicationRecord
   
   #通知機能
   has_many :notifications, dependent: :destroy
+
   def create_notification_by(current_user)
     notification = current_user.active_notifications.new(
       item_id: id,
@@ -46,12 +47,12 @@ class Item < ApplicationRecord
 
   def create_notification_comment!(current_user, comment_id)
     # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
-    temp_ids = Comment.select(:user_id).where(item_id: id).where.not(user_id: current_user.id).distinct
-    temp_ids.each do |temp_id|
-      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    commenter_ids = Comment.select(:user_id).where(item_id: id).where.not(user_id: current_user.id).distinct
+    commenter_ids.each do |commenter|
+      save_notification_comment!(current_user, comment_id, commenter['user_id'])
     end
     # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+    save_notification_comment!(current_user, comment_id, user_id) if commenter_ids.blank?
   end
 
   def save_notification_comment!(current_user, comment_id, visited_id)
